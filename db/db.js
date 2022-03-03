8a0c53a83e097925312975aecf5f4344b0ce5985
Robert Albenda <ralbenda@gmail.com>
2022-03-03 10:28:46 -0500

    robs to do

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@
 
 const Sequelize = require("sequelize");
 const db = new Sequelize(
-  process.env.DATABASE || "postgres://localhost/to_do_list_1"
+  process.env.DATABASE_URL || "postgres://localhost/to_do_list_1"
 );
 
 
